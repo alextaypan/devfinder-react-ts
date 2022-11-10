f014ecc8152be9d00e24bce9915e222bd5d2638a
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-11-10 14:42:11 +0200

    feat: complete project

---
+++ src/mock/index.ts
@@ -0,0 +1,16 @@
+import { LocalGithubUser } from "types";
+
+export const defaultUser: LocalGithubUser = {
+  login: "alextaypan",
+  avatar: "https://avatars.githubusercontent.com/u/91522978?v=4",
+  name: "Oleksandr Yakob",
+  company: null,
+  blog: null,
+  location: "Kyiv, Ukraine",
+  bio: "React developer",
+  twitter: "yakoboleksandr",
+  repos: 41,
+  followers: 1,
+  following: 1,
+  created: "2021-09-28T06:42:49Z",
+};
